bcbe065399037dc4727b0309d2ccae43f071d7e3
Nicholas Parham <62166636+scythetrigger@users.noreply.github.com>
2022-02-10 09:40:04 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'amplParser',
-    version = '0.0.0',    
+    version = '0.0.3',    
     description = 'A python module to create parse AMPL output into JSON/dictionaries.',
     url = 'https://github.com/scythetrigger/amplParserpy',
     author = 'Nicholas Parham',
